37370b18ad028da2f4f07fc12ff83c3c94aff1b5
Alik8885 <micyshuro@mail.ru>
2023-12-09 19:24:40 +0300

    Готовлюсь к защите^^

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.get("/board", (req, res) => {
+    res.render("board.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
